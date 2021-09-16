ee2d4a0968005a0dee1da4bed9d11ded7debff4a
shin <shin@naver.com>
2021-09-16 11:44:01 +0900

    커스텀exception 처리

--- src/main/java/com/cos/blogapp/handler/GlobalExceptionHandler.java
+++ src/main/java/com/cos/blogapp/handler/GlobalExceptionHandler.java
@@ -1,11 +1,10 @@
 package com.cos.blogapp.handler;
 
-import java.util.NoSuchElementException;
-
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.cos.blogapp.handler.ex.MyNotFoundException;
 import com.cos.blogapp.util.Script;
 
 // 1. Exception 핸들링, 2. @Controller의 역할까지 한다.
@@ -13,9 +12,9 @@ import com.cos.blogapp.util.Script;
 public class GlobalExceptionHandler {
 	
 	//@ResponsBody 데이터를 리턴하게 해준다. 
-	@ExceptionHandler(value = NoSuchElementException.class)
-	public @ResponseBody String error1(NoSuchElementException e) {
+	@ExceptionHandler(value = MyNotFoundException.class)
+	public @ResponseBody String error1(MyNotFoundException e) {
 		System.out.println("오류터졌어 : " + e.getMessage());
-		return Script.href("/", "게시글을 찾을 수 없습니다.");
+		return Script.href("/", e.getMessage());
 	}
 }
